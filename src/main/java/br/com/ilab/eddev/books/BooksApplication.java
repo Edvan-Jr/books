6040cad8cde03b24a25705c9fdf7f0b35a5d2538
Edvan-Jr <edvan.hd@gmail.com>
2022-04-20 10:25:43 -0300

    Application initialized

---
+++ src/main/java/br/com/ilab/eddev/books/BooksApplication.java
@@ -0,0 +1,13 @@
+package br.com.ilab.eddev.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
